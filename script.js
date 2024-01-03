4a4d8c8d1d9f86d3ad56b6fd7de6a238c15ee074
Likssss <148707565+Likssss@users.noreply.github.com>
2024-01-03 14:10:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ document.getElementById('envelope').addEventListener('click', function() {
     var note = document.getElementById('note');
     var popup = document.getElementById('popup');
     var question = document.getElementById('question');
-    var text = "This might be made in a rush but apparently it also racked my brain, well that aside i'm sorry for everything syg i didn't mean to keep making you mad or unhappy, please forgive me for always making you unhappy and mad, i'm sorry for not being the best for you, i'll try and be better so please forgive me yes? don't get mad at me anymore :( i'm sorry and i love you
-";
+    var text = "This might be made in a rush but apparently it also racked my brain, well that aside i'm sorry for everything syg i didn't mean to keep making you mad or unhappy, please forgive me for always making you unhappy and mad, i'm sorry for not being the best for you, i'll try and be better so please forgive me yes? don't get mad at me anymore :( i'm sorry and i love you";
     var words = text.split(' ');
     var i = 0;
 
